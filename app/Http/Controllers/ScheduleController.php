1b86ee223253d32dd906eb1aff56985dd7b208b7
ferdy <kpferdy@gmail.com>
2026-09-18 16:34:53 +0700

    fix: resolve 500 error on schedule index page null relation

--- app/Http/Controllers/ScheduleController.php
+++ app/Http/Controllers/ScheduleController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Employee;
-use App\Models\Schedule;
+use App\Models\EmployeeSchedule as Schedule;
 use App\Models\Shift;
 use App\Models\Site;
 use App\Models\SitePattern;
